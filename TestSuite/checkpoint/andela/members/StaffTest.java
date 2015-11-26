47d2bb2466d07e91fa47d45ef43c016fa35eb3c2
andela-shassan <hassan.ayomon@andela.com>
2015-11-26 17:15:19 +0100

    complete the staff class

---
+++ TestSuite/checkpoint/andela/members/StaffTest.java
@@ -0,0 +1,22 @@
+package checkpoint.andela.members;
+
+import checkpoint.andela.main.Member;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Semiu on 26/11/2015.
+ */
+public class StaffTest extends Member {
+
+  @Test
+  public void testBorrowBook() throws Exception {
+
+  }
+
+  @Test
+  public void testReturnBook() throws Exception {
+
+  }
+}
---
+++ src/checkpoint/andela/members/Staff.java
@@ -0,0 +1,46 @@
+package checkpoint.andela.members;
+
+import checkpoint.andela.main.*;
+
+/**
+ * Created by Semiu on 26/11/2015.
+ * This class inherit Member class
+ * It also declare two additional properties:
+ * Staff-number and net-pay
+ */
+public class Staff extends Member {
+  private int staffNumber;
+  private double netPay;
+
+  // Default constructor
+  public Staff() {
+    super();
+    staffNumber = 0;
+    netPay = 0.0d;
+  }
+
+  // Parameterize constructor
+  public Staff(String names, char sex, String dateOfBirth, String email, String phoneNumber, int staffNumber, double netPay) {
+    super(names, sex, dateOfBirth, email, phoneNumber);
+    setStaffNumber(staffNumber);
+    setStaffNetpay(netPay);
+  }
+
+  // Setter and Getter for Staff Number
+  public void setStaffNumber(int staffNumber) {
+    this.staffNumber = staffNumber;
+  }
+
+  public int getStaffNumber() {
+    return staffNumber;
+  }
+
+  // Setter  and Getter for Staff Net-Pay
+  public void setStaffNetpay(double netPay) {
+    this.netPay = netPay;
+  }
+
+  public double getStaffNetpay() {
+    return netPay;
+  }
+}
