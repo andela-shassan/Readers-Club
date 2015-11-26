9bd9f5a0edcd36d9563f97d06f8dd5bc673d94da
andela-shassan <hassan.ayomon@andela.com>
2015-11-26 15:26:31 +0100

    complete Book class test

--- TestSuite/checkpoint/andela/main/BookTest.java
+++ TestSuite/checkpoint/andela/main/BookTest.java
@@ -1,61 +1,46 @@
 package checkpoint.andela.main;
 
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 /**
  * Created by Semiu on 26/11/2015.
  */
 public class BookTest {
+  Book book1 = new Book("God are not to blame","Chinua Achebe", 5, "ISBN:08798-48");
+  Book book2 = new Book();
 
   @Test
   public void testSetBookTitle() throws Exception {
-
+    assertEquals(book2.getBookTitle(),null);
+    book2.setBookTitle("Die another day");
+    assertEquals(book2.getBookTitle(),"Die another day");
   }
 
   @Test
   public void testGetBookTitle() throws Exception {
-
+    assertEquals(book1.getBookTitle(),"God are not to blame");
+    assertEquals(book1.getNumberOfCopies(), 5);
   }
 
   @Test
   public void testSetBookAuthor() throws Exception {
-
+    book2.setBookAuthor("Wole Soyinka");
+    assertEquals(book2.getBookAuthor(),"Wole Soyinka");
   }
 
-  @Test
-  public void testGetBookAuthor() throws Exception {
-
-  }
-
-  @Test
-  public void testSetNumberOfCopies() throws Exception {
-
-  }
-
-  @Test
-  public void testGetNumberOfCopies() throws Exception {
-
-  }
-
-  @Test
-  public void testSetIsbnNumber() throws Exception {
-
-  }
-
-  @Test
-  public void testGetIsbnNumber() throws Exception {
-
-  }
 
   @Test
   public void testDecrementBook() throws Exception {
-
+    book1.decrementBook();
+    book2.decrementBook();
+    assertEquals(book1.getNumberOfCopies(), 4);
+    assertEquals(book2.getNumberOfCopies(), 0);
   }
 
   @Test
   public void testIncrementBook() throws Exception {
-
+    book1.incrementBook();
+    assertEquals(book1.getNumberOfCopies(), 6);
   }
 }
