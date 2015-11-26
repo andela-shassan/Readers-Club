27d77241d5e9b4d79588ba55f00ee1ce21a19812
andela-shassan <hassan.ayomon@andela.com>
2015-11-26 17:21:48 +0100

    Update Staff Tests

--- TestSuite/checkpoint/andela/members/StaffTest.java
+++ TestSuite/checkpoint/andela/members/StaffTest.java
@@ -1,22 +1,28 @@
 package checkpoint.andela.members;
 
-import checkpoint.andela.main.Member;
+import checkpoint.andela.main.Book;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 /**
  * Created by Semiu on 26/11/2015.
  */
-public class StaffTest extends Member {
+public class StaffTest {
+  Staff staff1 = new Staff();
+  Book book1 = new Book();
+
 
   @Test
   public void testBorrowBook() throws Exception {
-
+    book1.setNumberOfCopies(2);
+    staff1.borrowBook(book1);
+    assertEquals(book1.getNumberOfCopies(), 1);
   }
 
   @Test
   public void testReturnBook() throws Exception {
-
+    book1.setNumberOfCopies(1);
+    staff1.returnBook(book1);
+    assertEquals(book1.getNumberOfCopies(), 2);
   }
 }
